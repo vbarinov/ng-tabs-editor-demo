656f1796ce598d95259f7fa59f139bb4eb9c1550
Vlad B <vvbarinov@gmail.com>
2017-04-05 01:34:09 +0500

    More styling and fixes

--- src/tabs-app/values/FakeTabs.js
+++ src/tabs-app/values/FakeTabs.js
@@ -12,5 +12,14 @@ export default [
     label: `Прогнозы`,
     content: `Прогнозирование описывается здесь`,
     active: true
+  },
+  {
+    label: `Наше видео`,
+    content: `<div style="position:relative;height:0;padding-bottom:56.25%"><iframe src="https://www.youtube.com/embed/_ZXUCQc2Z78?ecver=2" width="640" height="360" frameborder="0" style="position:absolute;width:100%;height:100%;left:0" allowfullscreen></iframe></div>`
+  },
+  {
+    label: `Контакты`,
+    content: ``,
+    active: true
   }
 ];
